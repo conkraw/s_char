c0d7446b841ae6d2ce90f1a0cb1a8bdbd24c1151
conkraw <conkraw@gmail.com>
2024-11-06 21:52:01 -0500

    Update appy.py

--- appy.py
+++ appy.py
@@ -94,6 +94,8 @@ def combine_notes(assess_text, critical_care_reason, diagnoses, free_text_diag=N
     
     if ros_file == "None.docx":
         ros_paragraph = doc.add_paragraph()
+        ros_paragraph.paragraph_format.space_after = Pt(0)
+        ros_paragraph.paragraph_format.space_before = Pt(0)
         
     # Add Review of Systems section if a ROS file is selected (not empty)
     if ros_file != "None.docx":
